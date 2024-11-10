075820023bea80980541281c65345abf3472022a
Talia Friedman <taliafriedman22@gmail.com>
2024-11-10 11:22:17 +0200

    first push to see if gitignore

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        PUBLIC_DB_CONNECTION: "mongodb+srv://odeyacopperman:D0P7tYHEgzueAPPq@cluster0.1rf5i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    }
+};
 
 export default nextConfig;
+
+
+
+
