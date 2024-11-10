cddecf4a85e85fed355a7f473953f1a063750c3f
Tamar-Amar <amtamar747@gmail.com>
2024-11-10 14:29:21 +0200

    filled-recipes-from-atlas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,14 @@
+"use client";
 import NavBar from "@/components/NavBar";
-
-import AddRecipe from "@/components/AddRecipe";
-
 import GridRecipes from "@/components/GridRecipes";
 
-const recipes = [
-  {
-    id: "1",
-    url_image: "https://via.placeholder.com/150",
-    recipe_name: "Spaghetti Carbonara",
-    category: "Main Course",
-    instructions: "Boil pasta, fry pancetta, and mix with eggs and cheese.",
-  },
-  // Add more recipes
-];
-
 export default function Home() {
+
+
   return (
     <div>
       <NavBar />
-
-      <h1>hello world</h1>
-      <GridRecipes recipes={recipes} />
+      <GridRecipes/>
     </div>
   );
 }
