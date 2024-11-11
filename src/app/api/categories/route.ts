2bebbdb0fd408ea715df1e263f10499f034a0027
onico100 <142163577+onico100@users.noreply.github.com>
2024-11-11 11:22:26 +0200

    change close

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -6,6 +6,5 @@ import { getAllDocuments, connectDatabase } from "@/services/mongo";
 export async function GET(req: Request) {
   const client = await connectDatabase();
   const recipes = await getAllDocuments(client, "categories");
-  client.close();
   return NextResponse.json(recipes);
 }
