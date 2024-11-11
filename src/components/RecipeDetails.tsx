677a049b557987b0253293adc570996a47047838
Talia Friedman <taliafriedman22@gmail.com>
2024-11-11 10:08:47 +0200

    style for details

--- src/components/RecipeDetails.tsx
+++ src/components/RecipeDetails.tsx
@@ -1,6 +1,8 @@
 // RecipeDetails.tsx
 "use client";
 import React from "react";
+import styles from "@/styles/RecipeDetails.module.css";
+
 
 type RecipeDetailsProps = {
   isOpen: boolean;
@@ -23,17 +25,19 @@ export default function RecipeDetails({
   if (!isOpen) return null;
 
   return (
-    <div>
-      <button onClick={onClose}>Close</button>
-      <h2>{recipe.recipe_name}</h2>
-      <img src={recipe.url_image} alt={recipe.recipe_name} />
-      <p>Category: {recipe.categoryName}</p> {/* Show category name */}
-      <p>Instructions: {recipe.instructions}</p>
-      <ul>
-        {recipe.ingredients.map((ingredient, index) => (
-          <li key={index}>{ingredient}</li>
-        ))}
-      </ul>
+    <div className={styles.overlay}>
+      <div className={styles.popup}>
+        <button className={styles.closeButton} onClick={onClose}>&times;</button>
+        <h2 className={styles.recipeName}>{recipe.recipe_name}</h2>
+        <img className={styles.image} src={recipe.url_image} alt={recipe.recipe_name} />
+        <p>Category: {recipe.categoryName}</p> {/* Show category name */}
+        <p>Instructions: {recipe.instructions}</p>
+        <ul className={styles.ingredient}>
+          {recipe.ingredients.map((ingredient, index) => (
+            <li key={index}>{ingredient}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
