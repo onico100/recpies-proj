d505b99254453b83bf1b018d368a6975ce9db0ac
onico100 <142163577+onico100@users.noreply.github.com>
2024-11-14 09:40:24 +0200

    change star to heart

--- src/components/RecipeDetails.tsx
+++ src/components/RecipeDetails.tsx
@@ -1,8 +1,8 @@
 "use client";
 import React from "react";
-import { AiOutlineStar, AiFillStar } from "react-icons/ai";
 import styles from "@/styles/RecipeDetails.module.css";
-
+import { FaRegHeart } from "react-icons/fa6";
+import { IoMdHeart } from "react-icons/io";
 
 type RecipeDetailsProps = {
   isOpen: boolean;
@@ -26,23 +26,29 @@ export default function RecipeDetails({
   isFavorite,
   onToggleFavorite,
 }: RecipeDetailsProps) {
-
   if (!isOpen) return null;
 
   return (
     <div className={styles.overlay}>
       <div className={styles.popup}>
-        <button className={styles.closeButton} onClick={onClose}>&times;</button>
+        <button className={styles.closeButton} onClick={onClose}>
+          &times;
+        </button>
         <div className={styles.content}>
-
           <div className={styles.info}>
             <h2 className={styles.name}>{recipe.recipe_name}</h2>
             <div className={styles.categoryContainer}>
               <p className={styles.category}>Category: {recipe.categoryName}</p>
               {isFavorite ? (
-                <AiFillStar className={styles.starIcon} onClick={onToggleFavorite} />
+                <IoMdHeart
+                  className={styles.starIcon}
+                  onClick={onToggleFavorite}
+                />
               ) : (
-                <AiOutlineStar className={styles.starIcon} onClick={onToggleFavorite} />
+                <FaRegHeart
+                  className={styles.starIcon}
+                  onClick={onToggleFavorite}
+                />
               )}
             </div>
             <p className={styles.ingredientsTitle}>Ingredients:</p>
@@ -52,10 +58,16 @@ export default function RecipeDetails({
               ))}
             </ul>
           </div>
-          <img className={styles.image} src={recipe.url_image} alt={recipe.recipe_name} />
+          <img
+            className={styles.image}
+            src={recipe.url_image}
+            alt={recipe.recipe_name}
+          />
         </div>
         <hr className={styles.divider} />
-        <p className={styles.instructions}><b>Instructions:</b> {recipe.instructions}</p>
+        <p className={styles.instructions}>
+          <b>Instructions:</b> {recipe.instructions}
+        </p>
       </div>
     </div>
   );
