652520f07258707f7170675d5050ec35bff9c6e5
onico100 <142163577+onico100@users.noreply.github.com>
2024-11-14 10:16:34 +0200

    local storage for zustang

--- src/stores/recipesStore.ts
+++ src/stores/recipesStore.ts
@@ -1,13 +1,22 @@
 import { create } from "zustand";
 import { Recipe } from "@/types/RecipeTypes";
+import { persist } from "zustand/middleware";
 
 export type RecipesStore = {
   recipes: Recipe[];
   setRecipes: (recipes: Recipe[]) => void;
 };
 
-export const useRecipesStore = create<RecipesStore>((set) => ({
-  recipes: [],
-  setRecipes: (recipes) => set({ recipes }),
-  console: console.log("recipes store created"),
-}));
+export const useRecipesStore = create<RecipesStore>()(
+  persist(
+    (set) => ({
+      recipes: [],
+      setRecipes: (recipes) => set({ recipes }),
+    }),
+    {
+      name: "recipes-storage", // Specify a storage name (optional)
+    }
+  )
+);
+
+console.log("recipes store created");
